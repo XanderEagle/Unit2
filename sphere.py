654e6db3bb35c974433b749089d62015823922fb
Xander <alexander.eagle@ssfs.org>
2019-09-18 14:40:32 -0400

    did math problems

---
+++ sphere.py
@@ -0,0 +1,10 @@
+volume_of_sphere = 4 / 3 * 3.14 * 5 ** 3
+print("volume of sphere is", volume_of_sphere)
+
+variable_numbooks = 60
+total_of_books = 24.95 * .6 * variable_numbooks + 3 + .75 * (variable_numbooks-1)
+print("total of books is", total_of_books)
+
+leave_house = 6.86
+return_time = 8.25 + 7.2 * 3 + leave_house
+print("return time is", return_time)
