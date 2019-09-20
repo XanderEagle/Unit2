31a60adbee0cba79c9cec1999559b762fd038edd
Xander <alexander.eagle@ssfs.org>
2019-09-20 08:56:26 -0400

    math problems with area, height,and width

--- sphere.py
+++ sphere.py
@@ -8,3 +8,20 @@ print("total of books is", total_of_books)
 leave_house = 6.86
 return_time = 8.25 + 7.2 * 3 + leave_house
 print("return time is", return_time)
+
+length_of_rectangle = 10
+width_of_rectangle = 6
+the_area_of_the_rectangle_is = length_of_rectangle * width_of_rectangle
+print("the area of the rectangle is", the_area_of_the_rectangle_is)
+
+height_of_triangle = 10
+base_of_triangle = 8
+the_area_of_the_triangle_is = height_of_triangle * base_of_triangle * 0.5
+print("the area of the triangle is", the_area_of_the_triangle_is)
+
+P = 10000
+n = 12
+r = 0.08
+t = 10
+A = P*(1+r/n)**(n*t)
+print("A", A)
