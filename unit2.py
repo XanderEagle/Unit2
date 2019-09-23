9b98a9544f467b51e3c988368282f9be2443eff2
Xander <alexander.eagle@ssfs.org>
2019-09-23 10:15:35 -0400

    added bot voice floats

---
+++ unit2.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+print("What's up", name, "my name is Herbert, but you can call me Herb. Where are you from?")
+place = input()
+print("Woah, I wish I lived in," , place, " instead of this computer.")
+
