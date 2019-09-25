ffa0a7e8b208af2015d51a0ed825d83b5e068064
Xander <alexander.eagle@ssfs.org>
2019-09-25 11:56:43 -0400

    finished bot convo

--- unit2.py
+++ unit2.py
@@ -8,9 +8,15 @@ print("That's so cool, my favorite number", my_number, "is three times bigger th
 car = input("What is your dream car?")
 print("Dang", name, "I see you. A", car, "is a great car.")
 price = float(input("How much does it cost?"))
-print("Golly", price, "is a lot of dinero. If you took out a loan, how many years would you make it be?")
+print("Golly", price, "is a lot of dinero. If you took out a loan, how many years would you make it?")
 years = float(input())
 print("What would the annual interest rate (percent) be?")
-amoutn_of_months_to_pay = years / 12
-interest_rate = float(input())
-monthly_cost = float(interest_rate * price) / (1 - (1 + monthly_cost)** - n)
+monthly_interest_rate = float(input())
+number_of_monthly_payments = years * 12
+monthly_interest_rate = monthly_interest_rate / 100 / 12
+monthly__cost = float(monthly_interest_rate * price) / (1 - (1 + monthly_interest_rate)**-number_of_monthly_payments)
+print("If you bought a", car, "your monthly cost would be,", monthly__cost, "per month.")
+total_cost = monthly__cost * 12 * years
+print("And the total cost would be,", total_cost)
+
+
